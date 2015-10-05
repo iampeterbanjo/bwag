399c43c17f4754714cd2715f39fae0020d262ffb
iampeterbanjo <peterbanjo@gmail.com>
2015-10-05 17:45:08 +0100

    Add templates

---
+++ main_rendering.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+	"path"
+	"log"
+)
+
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("listening on " + port)
+	http.HandleFunc("/", ShowBooks)
+	http.ListenAndServe(":" + port, nil)
+}
+
+func ShowBooks(w http.ResponseWriter, r *http.Request) {
+	book := Book{"Building Web Apps with Go", "Jeremy Saenz"}
+
+	fp := path.Join("templates", "index.html")
+	tmpl, err := template.ParseFiles(fp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.Execute(w, book); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
