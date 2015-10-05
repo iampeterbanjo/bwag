826e5d3e0ff70f70a3b6b257d2f3b747f1a58799
iampeterbanjo <peterbanjo@gmail.com>
2015-10-05 15:55:18 +0100

    Middleware

--- main.go
+++ main.go
@@ -1,56 +1,38 @@
+package main
+
 import (
-	"fmt"
+	"log"
 	"net/http"
-	"github.com/julienschmidt/httprouter"
+
+	"github.com/codegangsta/negroni"
+	"os"
 )
 
 func main() {
-	r := httprouter.New()
-	r.GET("/", HomeHandler)
-
-	// Posts collection
-	r.GET("/posts", PostsIndexHandler)
-	r.POST("/posts", PostsCreateHandler)
-
-	// Posts singular
-	r.GET("/posts/:id", PostShowHandler)
-	r.PUT("/posts/:id", PostUpdateHandler)
-	r.GET("/posts/:id/edit", PostEditHandler)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":" + port, r)
+	// Middleware stack
+	n := negroni.New(
+		negroni.NewRecovery(),
+		negroni.HandlerFunc(MyMiddleware),
+		negroni.NewLogger(),
+		negroni.NewStatic(http.Dir("public")),
+	)
+
+	n.Run(":" + port)
 }
 
-func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "Home")
-}
+func MyMiddleware(rw http.ResponseWriter, r * http.Request, next http.HandlerFunc) {
+	log.Println("Logging on the way there")
 
-func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "posts index")
-}
+	if r.URL.Query().Get("password") == "secret123" {
+		next(rw, r)
+	} else {
+		http.Error(rw, "Not Authorized", 401)
+	}
 
-func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "posts create")
-}
-
-func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	id := p.ByName("id")
-	fmt.Fprintln(rw, "showing post", id)
-}
-
-func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post update")
-}
-
-func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post delete")
-}
-
-func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-	fmt.Fprintln(rw, "post edit")
+	log.Println("Logging on the way back")
 }
---
+++ main_httproouter.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	r := httprouter.New()
+	r.GET("/", HomeHandler)
+
+	// Posts collection
+	r.GET("/posts", PostsIndexHandler)
+	r.POST("/posts", PostsCreateHandler)
+
+	// Posts singular
+	r.GET("/posts/:id", PostShowHandler)
+	r.PUT("/posts/:id", PostUpdateHandler)
+	r.GET("/posts/:id/edit", PostEditHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":" + port, r)
+}
+
+func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "Home")
+}
+
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts index")
+}
+
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "posts create")
+}
+
+func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	fmt.Fprintln(rw, "showing post", id)
+}
+
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post update")
+}
+
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post delete")
+}
+
+func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprintln(rw, "post edit")
+}
